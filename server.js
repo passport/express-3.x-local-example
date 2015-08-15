be52342d7a660e414020b82975975d3ea958891f
Jared Hanson <jaredhanson@gmail.com>
2015-08-15 11:36:03 -0700

    Add comments.

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ var Strategy = require('passport-local').Strategy;
 var db = require('./db');
 
 
-// Configure the Local strategy for use by Passport.
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
 passport.use(new Strategy(
   function(username, password, cb) {
     db.users.findByUsername(username, function(err, user) {
@@ -17,6 +22,12 @@ passport.use(new Strategy(
 
 
 // Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
 passport.serializeUser(function(user, cb) {
   cb(null, user.id);
 });
